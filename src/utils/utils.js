ceb04888f945741dd4430fcdc575ce7a576e21fc
rcbabahin <rcbabahin@yandex.ru>
2023-11-30 22:03:17 +0300

    changed csvToJson function to accept float number with ether dot or comma

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,22 +1,23 @@
 export const csvToJson = (csv) => {
-    let rows = csv.split('\r\n');
-    let replaceSymbol = ',';
+    let rows = csv.split('\n');
+
+    if (rows[0].includes('\r'))
+        rows = rows.map(row => row.slice(0, -1))
     // define field delimiter, four options are available: comma, semicolon, space and tab
     const fieldDelimiter = rows[0].includes(',') ? ',' : rows[0].includes(';') ? ';' : rows[0].includes('\t') ? '\t' : ' ';
+    
+    // clio data are only numbers, there are no strings in it
+    // so string delimiter exist only when we use field delimiter == ',' and have ',' as float number delimiter
+    // define string delimiter, but if there are no float number in first row there will be an error
+    const stringDelimiter = rows[1].includes('\'') ? '\'' : rows[1].includes('"') ? '"' : 'no string in row';
 
-    if (fieldDelimiter === ',') {
-        // clio data are only numbers, there are no strings in it
-        // so string delimiter exist only when we use field delimiter == ','
-        // define string delimiter, but if there are no float number in first row there will be an error
-        const stringDelimiter = rows[1].includes('\'') ? '\'' : '"';
+    if (stringDelimiter !== 'no string in row') {
         // regexp to find and replace comma (,) symbol in float numbers with * symbol
         const regexp = new RegExp(`${stringDelimiter}(.*?),(.*?)${stringDelimiter}`, 'g');
-        // update rows array with replaced comma symbol in folat numbers 
-        rows = rows.map(r => r.replace(regexp, (match, p1, p2) => `${p1}*${p2}`));
-        // change replace symbol for future needs
-        replaceSymbol = '*'
+        // update rows array with replaced comma symbol in float numbers 
+        rows = rows.map(row => row.replace(regexp, (match, p1, p2) => `${p1}.${p2}`));
     }
-        
+
     const header = rows[0].split(fieldDelimiter);
 
     const json = [];
@@ -27,12 +28,12 @@ export const csvToJson = (csv) => {
         const obj = {};
 
         for (let j = 0; j < header.length; j++) {
-            obj[header[j]] = parseFloat(values[j].replace(replaceSymbol, '.'));
+            obj[header[j]] = parseFloat(values[j].replace(',', '.'));
         }
 
         json.push(obj);
     }
-
+    console.log(json)
     return json;
 };
 
